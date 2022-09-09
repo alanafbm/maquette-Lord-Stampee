77358b3b9847480b3712e9db7a5e66612a8390e3
alanamoraes <alanaaaa@hotmail.com>
2022-09-09 15:45:39 -0400

    change login to PHP-MVC

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// URL de base du serveur
+const BASE_SERVEUR = 'http://leila.localhost:8080/';
+
+// Config de la BD
+const BD_HOTE = 'localhost';
+const BD_UTIL = 'leilaadimn';
+const BD_MDP = '@B1!34!fGpDkNz5c';
+const BD_NOM = 'leila';
+
+//Module par default
+const MODULE_DEFAUT = 'utilisateur';
